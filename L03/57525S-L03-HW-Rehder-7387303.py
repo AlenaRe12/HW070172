014965bf6107708183566229afcf5e53bf573246
Alena Rehder <93519465+AlenaRe12@users.noreply.github.com>
2021-11-11 22:12:04 +0100

    Add files via upload

---
+++ L03/57525S-L03-HW-Rehder-7387303.py
@@ -0,0 +1,279 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#Zelle Chapter 2
+#convert.py
+
+============= RESTART: C:/Users/rehde/Desktop/python/fahrenheit.py =============
+What is the Celsius temperature? 22
+The temperature is 71.6 degrees Fahrenheit.
+main()
+What is the Celsius temperature? 0
+The temperature is 32.0 degrees Fahrenheit.
+32
+32
+"Hello"
+'Hello'
+"32"
+'32'
+x = 5
+x
+5
+print(x)
+5
+print(3+4)
+7
+print("The answer is", 3 + 4)
+The answer is 7
+print()
+
+print ("The answer ist", end=" ")
+The answer ist 
+print("The answer is", end=" ")
+The answer is 
+print (3+4)
+7
+myVar = 0
+myVar
+0
+myVar = 7
+myVar
+7
+maVar = myVar + 1
+myVar
+7
+maVar
+8
+name = input("Enter your name: ")
+Enter your name: Alena
+name
+'Alena'
+ans = eval(input("Enter an expression: "))
+Enter an expression: 3 + 4* 5
+print(ans)
+23
+
+================ RESTART: C:/Users/rehde/Desktop/python/avg2.py ================
+This program computes the average to two exam scores.
+Enter two scores seperated by a comma: 86, 92
+The average of the scores is: 89.0
+for i in [0, 1, 2, 3]:
+    print(i)
+
+    
+0
+1
+2
+3
+
+for odd in [1, 3, 5, 7, 9]:
+    print(odd * odd)
+
+    
+1
+9
+25
+49
+81
+list(range(10))
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+=============== RESTART: C:/Users/rehde/Desktop/python/futval.py ===============
+
+=============== RESTART: C:/Users/rehde/Desktop/python/futval.py ===============
+This program calculates the future value
+ of an annualized investment.
+Enter the initial principal: 50
+Enter the annual interest rate in perecents: 3
+Enter the length of investment in years: 7
+Enter the amount to be deposited each month: 20
+The value in 7 years is 1900.4846166702036
+for i in range(5):
+    print(i * i)
+
+    
+0
+1
+4
+9
+16
+
+for d in [3,1,4,1,5]:
+    print(d, end=" ")
+
+    
+3 1 4 1 5 
+for i in range(4):
+    print("hello")
+
+    
+hello
+hello
+hello
+hello
+for i in range(5):
+    print(i, 2**i)
+
+    
+0 1
+1 2
+2 4
+3 8
+4 16
+
+============= RESTART: C:/Users/rehde/Desktop/python/fahrenheit.py =============
+Welcome! This program will kindly convert 
+ your degrees Celsius into Fahrenheit.
+What is the Celsius temperature? 0
+The temperature is  -17.77777777777778 degrees Fahrenheit.
+# Exercise 1
+
+============= RESTART: C:/Users/rehde/Desktop/python/fahrenheit.py =============
+Welcome! This program will kindly convert 
+ your degrees Celsius into Fahrenheit.
+What is the Celsius temperature? 0
+The temperature is  -17.77777777777778 degrees Fahrenheit.
+# Exercise 2
+
+============= RESTART: C:/Users/rehde/Desktop/python/fahrenheit.py =============
+Welcome! This program will kindly convert 
+ your degrees Celsius into Fahrenheit.
+What is the Celsius temperature? 0
+The temperature is  -17.77777777777778 degrees Fahrenheit.
+Press the <Enter> key to quit.
+# Exercise 3
+
+================ RESTART: C:/Users/rehde/Desktop/python/avg2.py ================
+This program computes the average of three exam scores.
+Enter three scores separated by commas: 55, 67, 89
+The average of the scores is: 70.33333333333333
+# Exercise 4
+
+============= RESTART: C:/Users/rehde/Desktop/python/fahrenheit.py =============
+Welcome! This program will kindly convert 
+ your degrees Celsius into Fahrenheit 5 different times.
+What is the Celsius temperature? 1
+The temperature is  -17.22222222222222 degrees Fahrenheit.
+What is the Celsius temperature? 2
+The temperature is  -16.666666666666668 degrees Fahrenheit.
+What is the Celsius temperature? 3
+The temperature is  -16.11111111111111 degrees Fahrenheit.
+What is the Celsius temperature? 4
+The temperature is  -15.555555555555555 degrees Fahrenheit.
+What is the Celsius temperature? 5
+The temperature is  -15.0 degrees Fahrenheit.
+#Exercise 5
+
+============= RESTART: C:/Users/rehde/Desktop/python/fahrenheit.py =============
+0 32.0
+10 50.0
+20 68.0
+30 86.0
+40 104.0
+50 122.0
+60 140.0
+70 158.0
+80 176.0
+90 194.0
+100 212.0
+# Exercise 6
+
+=============== RESTART: C:/Users/rehde/Desktop/python/futval.py ===============
+This program calculates the future value
+ of a 10-year investment.
+Enter the initial principal: 200
+Enter the annual interest rate in perecents: 12
+Enter the length of investment in years: 8
+The value in 8 years is 495.1926352589622
+
+=============== RESTART: C:/Users/rehde/Desktop/python/futval.py ===============
+This program calculates the future value
+ of a 10-year investment.
+Enter the initial principal: 1000
+Enter the annual interest rate in perecents: .2
+Enter the length of investment in years: 10
+The value in 10 years is 1020.1809633680776
+
+=============== RESTART: C:/Users/rehde/Desktop/python/futval.py ===============
+This program calculates the future value
+ of an annualized investment.
+Enter the initial principal: 200
+Enter the annual interest rate in perecents: 12
+Enter the length of investment in years: 8
+Enter the amount to be deposited each month: 100
+The value in 8 years is 15254.824398207063
+# Exercise 7
+
+=============== RESTART: C:/Users/rehde/Desktop/python/futval.py ===============
+This program calculates the future value
+ of an annualized investment.
+Enter the initial principal: 1000
+Enter the annual interest rate in perecents: .2
+Enter the length of investment in years: 10
+Enter the amount to be deposited each month: 100
+The value in 10 years is 13128.758984214552
+
+=============== RESTART: C:/Users/rehde/Desktop/python/futval.py ===============
+This program calculates the future value
+ of an investment compounded quarterly.
+Enter the initial principal: 1000
+Enter the annual interest rate in perecents: .3
+Enter the length of investment in years: 10
+Enter the amount to be deposited each month: 10
+The value in 10 years is 5901.3144989104
+# Exercise 8
+
+=============== RESTART: C:/Users/rehde/Desktop/python/futval.py ===============
+This program calculates the future value
+ of an investment compounded quarterly.
+Enter the initial principal: 1000
+Enter the annual interest rate in perecents: .3
+Enter the length of investment in years: 10
+Enter the amount to be deposited each month: 10
+The value in 10 years is 5901.3144989104
+# Exercise 9
+
+============= RESTART: C:/Users/rehde/Desktop/python/fahrenheit.py =============
+Welcome! This program will kindly convert 
+ your degrees Fahrenheit into Celsius.
+What is the Fahrenheit temperature? 120
+The temperature is  48.888888888888886 degrees Fahrenheit.
+
+=============== RESTART: C:/Users/rehde/Desktop/python/meilen.py ===============
+Welcome! This program will kindly convert 
+ your distance in kilometers to miles.
+What is the distance in km? 120
+The distance is  74.4 miles.
+
+============= RESTART: C:/Users/rehde/Desktop/python/fahrenheit.py =============
+Welcome! This program will kindly convert 
+ your degrees Fahrenheit into Celsius.
+What is the Fahrenheit temperature? 12
+The temperature is  -11.11111111111111 degrees Fahrenheit.
+# Exercise 10
+
+================= RESTART: C:/Users/rehde/Desktop/python/kg.py =================
+Welcome! This program will kindly convert 
+ your weight in kilogram to pounds.
+What is the weight in kg? 120
+The weight is  264.48 pounds.
+# Exercise 11
+main()
+Welcome! This program will kindly convert 
+ your weight in kilogram to pounds.
+What is the weight in kg? 100
+The weight is  220.4 pounds.
+# Exercise 12
+
+============= RESTART: C:/Users/rehde/Desktop/python/calculator.py =============
+Calculator
+3 * 2
+6
+
+Traceback (most recent call last):
+  File "C:/Users/rehde/Desktop/python/calculator.py", line 9, in <module>
+    main()
+  File "C:/Users/rehde/Desktop/python/calculator.py", line 6, in main
+    x = eval(input())
+  File "<string>", line 0
+    
+SyntaxError: unexpected EOF while parsing
