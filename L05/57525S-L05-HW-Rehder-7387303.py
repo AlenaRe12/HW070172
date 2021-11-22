08043efb8ec481d9fc01c6febd4a9f76f5fa8ab6
Alena Rehder <93519465+AlenaRe12@users.noreply.github.com>
2021-11-22 11:12:50 +0100

    Add files via upload

---
+++ L05/57525S-L05-HW-Rehder-7387303.py
@@ -0,0 +1,281 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+# Homework Chapter 6
+# Exercise 1
+
+============ RESTART: C:/Users/rehde/Desktop/python/Oldmacdonald.py ============
+Old MacDonald had a farm, Ee-igh, Ei-igh, Oh!
+And on this farm he had a(n) pig, Ee-igh, Ee-igh, Oh!
+With a(n) oink, oink here, and a(n) oink, oink there.
+Here a(n) oink, there a(n) oink, everywhere a(n) oink, oink.
+Old MacDonald had a farm, Ee-igh, Ei-igh, Oh!
+
+Old MacDonald had a farm, Ee-igh, Ei-igh, Oh!
+And on this farm he had a(n) cow, Ee-igh, Ee-igh, Oh!
+With a(n) moo, moo here, and a(n) moo, moo there.
+Here a(n) moo, there a(n) moo, everywhere a(n) moo, moo.
+Old MacDonald had a farm, Ee-igh, Ei-igh, Oh!
+
+Old MacDonald had a farm, Ee-igh, Ei-igh, Oh!
+And on this farm he had a(n) chicken, Ee-igh, Ee-igh, Oh!
+With a(n) cheep, cheep here, and a(n) cheep, cheep there.
+Here a(n) cheep, there a(n) cheep, everywhere a(n) cheep, cheep.
+Old MacDonald had a farm, Ee-igh, Ei-igh, Oh!
+
+Old MacDonald had a farm, Ee-igh, Ei-igh, Oh!
+And on this farm he had a(n) frog, Ee-igh, Ee-igh, Oh!
+With a(n) ribbit, ribbit here, and a(n) ribbit, ribbit there.
+Here a(n) ribbit, there a(n) ribbit, everywhere a(n) ribbit, ribbit.
+Old MacDonald had a farm, Ee-igh, Ei-igh, Oh!
+
+Old MacDonald had a farm, Ee-igh, Ei-igh, Oh!
+And on this farm he had a(n) goat, Ee-igh, Ee-igh, Oh!
+With a(n) bah, bah here, and a(n) bah, bah there.
+Here a(n) bah, there a(n) bah, everywhere a(n) bah, bah.
+Old MacDonald had a farm, Ee-igh, Ei-igh, Oh!
+# Exercise 2
+
+================ RESTART: C:/Users/rehde/Desktop/python/ants.py ================
+The ants go marching one by one, hurrah! hurrah!
+The ants go marching one by one, hurrah! hurrah!
+The ants go marching one by one,
+The little one stops to suck his thumb,
+And they all go marching down...
+Into the ground...
+To get out...
+Of the rain.
+Boom! Boom! Boom!
+
+The ants go marching two by two, hurrah! hurrah!
+The ants go marching two by two, hurrah! hurrah!
+The ants go marching two by two,
+The little one stops to tie his shoe,
+And they all go marching down...
+Into the ground...
+To get out...
+Of the rain.
+Boom! Boom! Boom!
+
+The ants go marching three by three, hurrah! hurrah!
+The ants go marching three by three, hurrah! hurrah!
+The ants go marching three by three,
+The little one stops to take a pee,
+And they all go marching down...
+Into the ground...
+To get out...
+Of the rain.
+Boom! Boom! Boom!
+
+The ants go marching four by four, hurrah! hurrah!
+The ants go marching four by four, hurrah! hurrah!
+The ants go marching four by four,
+The little one stops to shut the door,
+And they all go marching down...
+Into the ground...
+To get out...
+Of the rain.
+Boom! Boom! Boom!
+
+The ants go marching five by five, hurrah! hurrah!
+The ants go marching five by five, hurrah! hurrah!
+The ants go marching five by five,
+The little one stops to call the hive,
+And they all go marching down...
+Into the ground...
+To get out...
+Of the rain.
+Boom! Boom! Boom!
+
+The ants go marching six by six, hurrah! hurrah!
+The ants go marching six by six, hurrah! hurrah!
+The ants go marching six by six,
+The little one stops to pick up sticks,
+And they all go marching down...
+Into the ground...
+To get out...
+Of the rain.
+Boom! Boom! Boom!
+
+The ants go marching seven by seven, hurrah! hurrah!
+The ants go marching seven by seven, hurrah! hurrah!
+The ants go marching seven by seven,
+The little one stops to look to heaven,
+And they all go marching down...
+Into the ground...
+To get out...
+Of the rain.
+Boom! Boom! Boom!
+
+The ants go marching eight by eight, hurrah! hurrah!
+The ants go marching eight by eight, hurrah! hurrah!
+The ants go marching eight by eight,
+The little one stops to make a date,
+And they all go marching down...
+Into the ground...
+To get out...
+Of the rain.
+Boom! Boom! Boom!
+
+The ants go marching nine by nine, hurrah! hurrah!
+The ants go marching nine by nine, hurrah! hurrah!
+The ants go marching nine by nine,
+The little one stops to get outta line,
+And they all go marching down...
+Into the ground...
+To get out...
+Of the rain.
+Boom! Boom! Boom!
+
+The ants go marching ten by ten, hurrah! hurrah!
+The ants go marching ten by ten, hurrah! hurrah!
+The ants go marching ten by ten,
+The little one stops to sing it again,
+And they all go marching down...
+Into the ground...
+To get out...
+Of the rain.
+Boom! Boom! Boom!
+
+#Exercise 3
+
+=============== RESTART: C:/Users/rehde/Desktop/python/radius.py ===============
+Input a value for the radius: 22
+A sphere with radius 22 has volume 44602.238 and surface area 6082.123 .
+# Exercise 4
+
+================= RESTART: C:/Users/rehde/Desktop/python/sum.py ================
+Enter a value: 34
+The sum of the first 34 natural numbers is 595, and the sum of the
+sum of the first 34 cubes is 354025
+# Exercise 5
+
+=============== RESTART: C:/Users/rehde/Desktop/python/pizza2.py ===============
+Please enter the cost of the Pizza, and how big it was: 12, 30
+The pizza has an area of 706.86 square inches
+The price per square inch is about $0.02.
+# Exercise 6
+
+============== RESTART: C:/Users/rehde/Desktop/python/triangle4.py =============
+Input the 3 sides of the triangle, separated by commas: 
+    
+Traceback (most recent call last):
+  File "C:/Users/rehde/Desktop/python/triangle4.py", line 11, in <module>
+    main()
+  File "C:/Users/rehde/Desktop/python/triangle4.py", line 8, in main
+    a, b, c = eval(input("Input the 3 sides of the triangle, separated by commas: "))
+  File "<string>", line 0
+    
+SyntaxError: unexpected EOF while parsing
+main()
+Input the 3 sides of the triangle, separated by commas: Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    main()
+  File "C:/Users/rehde/Desktop/python/triangle4.py", line 8, in main
+    a, b, c = eval(input("Input the 3 sides of the triangle, separated by commas: "))
+  File "<string>", line 0
+    
+SyntaxError: unexpected EOF while parsing
+
+= RESTART: C:/Users/rehde/AppData/Local/Programs/Python/Python310/triangle5.py =
+Traceback (most recent call last):
+  File "C:/Users/rehde/AppData/Local/Programs/Python/Python310/triangle5.py", line 2, in <module>
+    from graphics import *
+ModuleNotFoundError: No module named 'graphics'
+
+============== RESTART: C:/Users/rehde/Desktop/python/triangle4.py =============
+Input the 3 sides of the triangle, separated by commas: 2,4,5
+The area of this triangle is:  3.8
+# Exercise 7
+
+= RESTART: C:/Users/rehde/AppData/Local/Programs/Python/Python310/fibonacci.py =
+What number in the Fibonnaci sequence would you like to see?: 4
+The 4 Fibonnaci number is 8.
+# Exercise 8
+
+=== RESTART: C:/Users/rehde/AppData/Local/Programs/Python/Python310/guess.py ===
+Find the square root of: 89
+How many iterations of Newton's formula would you like?: 10
+23.25
+13.538978494623656
+10.056295561220296
+9.453236495345585
+9.434000742748433
+9.433981132076987
+9.433981132056605
+9.433981132056605
+9.433981132056605
+9.433981132056605
+# Chapter 7
+# Exercise 1
+
+==== RESTART: C:/Users/rehde/AppData/Local/Programs/Python/Python310/wage.py ===
+How many hours worked, and how much per hour? 45, 12
+570.0
+# Exercise 2
+
+=== RESTART: C:/Users/rehde/AppData/Local/Programs/Python/Python310/grade.py ===
+What is the number grade? 3
+C
+
+=== RESTART: C:/Users/rehde/AppData/Local/Programs/Python/Python310/grade2.py ==
+B
+
+
+=== RESTART: C:/Users/rehde/AppData/Local/Programs/Python/Python310/grade3.py ==
+Exam Grader
+
+Enter the score: 67
+The grade is D
+# Exercise 3
+
+=== RESTART: C:/Users/rehde/AppData/Local/Programs/Python/Python310/grade3.py ==
+Exam Grader
+
+Enter the score: 56
+The grade is F
+# Exercise 4
+
+== RESTART: C:/Users/rehde/AppData/Local/Programs/Python/Python310/credeits.py =
+How many credits do you have? 20
+Junior
+# Exercise 5
+
+==== RESTART: C:/Users/rehde/AppData/Local/Programs/Python/Python310/bmi.py ====
+What is your weight (in pounds) and height (in inches)? 150, 60
+You are above the arbitrary healthy range.
+# Exercise 6
+
+=== RESTART: C:/Users/rehde/AppData/Local/Programs/Python/Python310/speed.py ===
+You have not committed a speed violation.
+
+=== RESTART: C:/Users/rehde/AppData/Local/Programs/Python/Python310/speed.py ===
+Speeding fine calculator
+
+Enter the speed limit 150
+Enter the clocked speed 170
+Fine $350.00
+# Exercise 7
+
+= RESTART: C:/Users/rehde/AppData/Local/Programs/Python/Python310/babysitter.py
+Babysitting Calculator
+
+Enter times using 24 hour format (e.g. 8 pm is 20:00)
+Starting time (hh:mm): 13:00
+Ending time (hh:mm): 23:30
+Total payment due: $24.38
+# Exercise 8
+
+== RESTART: C:/Users/rehde/AppData/Local/Programs/Python/Python310/Senator.py ==
+How many years have you been a U.S. Citizen? 10
+How old are you? 44
+You are elibible to serve as a U.S. Senator, or a member of the House.
+#Exercise 9
+
+=== RESTART: C:/Users/rehde/AppData/Local/Programs/Python/Python310/easter.py ==
+Easter falls on April 1.
+
+=== RESTART: C:/Users/rehde/AppData/Local/Programs/Python/Python310/easter.py ==
+Easter Calculator for 1982-2048
+
+Enter the year: 2022
+Easter is on April 17
